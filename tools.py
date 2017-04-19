b5122a204bbdcc8d04d95b9f4f580587234a58be
Jason R. Jones <slightlynybbled@gmail.com>
2017-04-19 08:00:12 -0400

    Added callbacks to the dropdown

--- tools.py
+++ tools.py
@@ -2,11 +2,41 @@ import tkinter as tk
 
 
 class DropDown(tk.OptionMenu):
-    def __init__(self, parent, options):
+    """
+    Classic drop down entry
+    
+    Example use:
+        # create the dropdown and grid
+        dd = DropDown(root, ['one', 'two', 'three'])
+        dd.grid()
+    
+        # define a callback function that retrieves the currently selected option
+        def callback():
+            print(dd.get())
+            
+        # add the callback function to the dropdown
+        dd.add_callback(callback)
+    """
+    def __init__(self, parent, options: list):
+        """
+        Constructor for drop down entry
+        
+        :param parent: the tk parent frame
+        :param options: a list containing the drop down options
+        :param callback: a function to be executed when the dropdown changes
+        """
         self.var = tk.StringVar(parent)
         self.var.set(options[0])
 
         self.option_menu = tk.OptionMenu.__init__(self, parent, self.var, *options)
+
+        self.callback = None
+
+    def add_callback(self, callback):
+        def internal_callback(*args):
+            callback()
+
+        self.var.trace("w", internal_callback)
 
     def get(self):
         return self.var.get()
@@ -16,5 +46,10 @@ if __name__ == '__main__':
 
     dd = DropDown(root, ['one', 'two', 'three'])
     dd.grid()
+
+    def callback():
+        print(dd.get())
+    dd.add_callback(lambda: print(dd.get()))
+
 
     root.mainloop()
