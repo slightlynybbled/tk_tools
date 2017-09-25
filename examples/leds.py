60bfc11146a43a6201dc2ddd9000e1c1d602ba43
Jason R. Jones <slightlynybbled@gmail.com>
2017-09-25 11:47:57 -0400

    Added yellow, added on/off colors for all but grey

--- examples/leds.py
+++ examples/leds.py
@@ -1,15 +1,17 @@
 import tkinter as tk
 import tk_tools
 
-import time
-
 root = tk.Tk()
 
 led = tk_tools.Led(root, size=50)
 led.pack()
 
-btn_red = tk.Button(root, text='red', command=led.to_red).pack(fill=tk.X)
-btn_green = tk.Button(root, text='green', command=led.to_green).pack(fill=tk.X)
-btn_grey = tk.Button(root, text='grey', command=led.to_grey).pack(fill=tk.X)
+tk.Button(root, text='red', command=led.to_red).pack(fill=tk.X)
+tk.Button(root, text='red on', command=lambda: led.to_red(True)).pack(fill=tk.X)
+tk.Button(root, text='green', command=led.to_green).pack(fill=tk.X)
+tk.Button(root, text='green on', command=lambda: led.to_green(True)).pack(fill=tk.X)
+tk.Button(root, text='green', command=led.to_yellow).pack(fill=tk.X)
+tk.Button(root, text='green on', command=lambda: led.to_yellow(True)).pack(fill=tk.X)
+tk.Button(root, text='grey', command=led.to_grey).pack(fill=tk.X)
 
 root.mainloop()
