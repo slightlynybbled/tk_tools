b470bf70e682e7a0d6037a335a94c3f8af1570c6
Aoki Ahishatsu <xeno1701@gmail.com>
2017-10-23 10:52:03 +0200

    Added ButtonGrid implementation + example Added new widgets in tk_tools/__init__.py

--- tk_tools/__init__.py
+++ tk_tools/__init__.py
@@ -1,14 +1,15 @@
 from tk_tools.visual import Dial, RotaryScale, Graph, Led
 from tk_tools.groups import EntryGrid, LabelGrid, \
-    KeyValueEntry, SpreadSheetReader
-from tk_tools.widgets import SmartOptionMenu, SmartSpinBox, SmartCheckbutton
+    KeyValueEntry, SpreadSheetReader, ButtonGrid
+from tk_tools.widgets import SmartOptionMenu, SmartSpinBox, \
+    SmartCheckbutton, ByteLabel
 
 from tk_tools.tkcalendar import Calendar
 
 from tk_tools.version import __version__
 
 __all__ = ['Dial', 'RotaryScale', 'Graph', 'Led',
-           'EntryGrid', 'LabelGrid', 'KeyValueEntry', 'SpreadSheetReader',
-           'SmartOptionMenu', 'SmartSpinBox', 'SmartCheckbutton',
-           'Calendar',
+           'EntryGrid', 'LabelGrid', 'ButtonGrid', 'KeyValueEntry',
+           'SpreadSheetReader', 'SmartOptionMenu', 'SmartSpinBox',
+           'SmartCheckbutton', 'Calendar', 'ByteLabel',
            '__version__']
