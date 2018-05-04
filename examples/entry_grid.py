520d45a495e8ca72c142609de184ebd830e70ab6
Jason R. Jones <slightlynybbled@gmail.com>
2018-05-04 07:09:51 -0400

    Updated example per feedback in #29

--- examples/entry_grid.py
+++ examples/entry_grid.py
@@ -24,19 +24,25 @@ if __name__ == '__main__':
 
     root = tk.Tk()
 
+    add_row_btn = tk.Button(root, text='Add Row', command=add_row)
+    add_row_btn.grid(row=0, column=0, columnspan=2, sticky='ew')
+
+    add_row_data_btn = tk.Button(root, text='Add Row (with data)', command=add_with_data)
+    add_row_data_btn.grid(row=1, column=0, columnspan=2, sticky='ew')
+
+    remove_row_btn = tk.Button(root, text='Remove Row')
+    remove_row_btn.grid(row=2, column=0, sticky='ew')
+
+    row_to_remove_entry = tk.Entry(root)
+    row_to_remove_entry.grid(row=2, column=1, sticky='ew')
+    row_to_remove_entry.insert(0, '0')
+
+    remove_row_btn.config(command=lambda: entry_grid.remove_row(int(row_to_remove_entry.get())))
+
+    read_btn = tk.Button(root, text='Read', command=read)
+    read_btn.grid(row=3, column=0, columnspan=2, sticky='ew')
+
     entry_grid = tk_tools.EntryGrid(root, 3, ['L0', 'L1', 'L2'])
-    entry_grid.grid(row=0, column=0)
-
-    add_row_btn = tk.Button(text='Add Row', command=add_row)
-    add_row_btn.grid(row=1, column=0, sticky='EW')
-
-    add_row_data_btn = tk.Button(text='Add Row (with data)', command=add_with_data)
-    add_row_data_btn.grid(row=2, column=0, sticky='EW')
-
-    remove_row_btn = tk.Button(text='Remove Row', command=remove_row)
-    remove_row_btn.grid(row=3, column=0, sticky='EW')
-
-    read_btn = tk.Button(text='Read', command=read)
-    read_btn.grid(row=4, column=0, sticky='EW')
+    entry_grid.grid(row=4, column=0, columnspan=2, sticky='ew')
 
     root.mainloop()
