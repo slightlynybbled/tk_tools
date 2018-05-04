4591d78e0fcf35579e27135885cb555323316984
ReblochonMasque <trustgold@gmail.com>
2018-05-04 15:18:58 +0800

    add if __name__ == '__main__':, renamed two functions, renamed one variable. As a consequence, one comment on parameters was also modified in Canvas, but it seemed appropriate.

--- examples/rotary_scale.py
+++ examples/rotary_scale.py
@@ -4,17 +4,17 @@ import tk_tools
 from tk_tools.images import rotary_gauge_volt
 
 
-def inc():
+def increment():
     global value
-    value += increment
+    value += increment_value
 
     p1.set_value(value)
     p2.set_value(value)
 
 
-def dec():
+def decrement():
     global value
-    value -= increment
+    value -= increment_value
 
     p1.set_value(value)
     p2.set_value(value)
@@ -36,18 +36,18 @@ if __name__ == '__main__':
 
     p2.grid(row=0, column=1)
 
-    increment = 1.0
+    increment_value = 1.0
     value = 0.0
 
     inc_btn = tk.Button(root,
-                        text='increment by {}'.format(increment),
-                        command=inc)
+                        text='increment_value by {}'.format(increment_value),
+                        command=increment)
 
     inc_btn.grid(row=1, column=0, columnspan=2, sticky='news')
 
     dec_btn = tk.Button(root,
-                        text='decrement by {}'.format(increment),
-                        command=dec)
+                        text='decrement by {}'.format(increment_value),
+                        command=decrement)
 
     dec_btn.grid(row=2, column=0, columnspan=2, sticky='news')
 
