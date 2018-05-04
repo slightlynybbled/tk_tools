e66331e591f8b2ac716e1c1d41efb95abec13f0e
ReblochonMasque <trustgold@gmail.com>
2018-05-04 15:11:53 +0800

    add if __name__ == '__main__':

--- examples/rotary_scale.py
+++ examples/rotary_scale.py
@@ -3,23 +3,6 @@ import tk_tools
 
 from tk_tools.images import rotary_gauge_volt
 
-root = tk.Tk()
-
-p1 = tk_tools.RotaryScale(root, max_value=100.0, size=100, unit='km/h')
-p1.grid(row=0, column=0)
-
-p2 = tk_tools.RotaryScale(root,
-                          max_value=100.0,
-                          size=100,
-                          needle_thickness=3,
-                          needle_color='black',
-                          img_data=rotary_gauge_volt)
-
-p2.grid(row=0, column=1)
-
-increment = 1.0
-value = 0.0
-
 
 def inc():
     global value
@@ -37,16 +20,35 @@ def dec():
     p2.set_value(value)
 
 
-inc_btn = tk.Button(root,
-                    text='increment by {}'.format(increment),
-                    command=inc)
+if __name__ == '__main__':
 
-inc_btn.grid(row=1, column=0, columnspan=2, sticky='news')
+    root = tk.Tk()
 
-dec_btn = tk.Button(root,
-                    text='decrement by {}'.format(increment),
-                    command=dec)
+    p1 = tk_tools.RotaryScale(root, max_value=100.0, size=100, unit='km/h')
+    p1.grid(row=0, column=0)
 
-dec_btn.grid(row=2, column=0, columnspan=2, sticky='news')
+    p2 = tk_tools.RotaryScale(root,
+                              max_value=100.0,
+                              size=100,
+                              needle_thickness=3,
+                              needle_color='black',
+                              img_data=rotary_gauge_volt)
 
-root.mainloop()
+    p2.grid(row=0, column=1)
+
+    increment = 1.0
+    value = 0.0
+
+    inc_btn = tk.Button(root,
+                        text='increment by {}'.format(increment),
+                        command=inc)
+
+    inc_btn.grid(row=1, column=0, columnspan=2, sticky='news')
+
+    dec_btn = tk.Button(root,
+                        text='decrement by {}'.format(increment),
+                        command=dec)
+
+    dec_btn.grid(row=2, column=0, columnspan=2, sticky='news')
+
+    root.mainloop()
