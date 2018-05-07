6fedbea3b058a7867b3172402618254e15dce847
Jason R. Jones <slightlynybbled@gmail.com>
2018-05-07 08:04:42 -0400

    Initializing elements test

--- test.py
+++ test.py
@@ -6,31 +6,34 @@ from tk_tools import *
 
 
 @pytest.fixture
-def create_root():
-    root = tk.Tk()
-    yield root
+def root():
+    root_frame = tk.Tk()
+    yield root_frame
 
 
-def test_init(create_root):
+def test_init(root):
     """
     Ensures that all elements of the GUI may be instantiated
     without errors.
     """
-    RotaryScale(create_root)
-    Gauge(create_root)
-    Graph(create_root, 0, 10, 0, 10, 0.1, 0.1)
-    Led(create_root)
-    EntryGrid(create_root, 3)
-    LabelGrid(create_root, 3)
-    ButtonGrid(create_root, 3)
-    KeyValueEntry(create_root, keys=['1', '2'])
-    SmartOptionMenu(create_root, ['1', '2'])
-    SmartSpinBox(create_root)
-    SmartCheckbutton(create_root)
-    Calendar(create_root)
-    MultiSlotFrame(create_root)
-    SevenSegmentDigits(create_root)
-    BinaryLabel(create_root)
-    ToolTip(BinaryLabel(create_root), 'some text')
+    RotaryScale(root).grid()
+    Gauge(root).grid()
+    Graph(root, 0, 10, 0, 10, 0.1, 0.1).grid()
+    Led(root).grid()
+    EntryGrid(root, 3).grid()
+    LabelGrid(root, 3).grid()
+    ButtonGrid(root, 3).grid()
+    KeyValueEntry(root, keys=['1', '2']).grid()
+    SmartOptionMenu(root, ['1', '2']).grid()
+    SmartSpinBox(root).grid()
+    SmartCheckbutton(root).grid()
+    Calendar(root).grid()
+    MultiSlotFrame(root).grid()
+    SevenSegmentDigits(root).grid()
+    BinaryLabel(root).grid()
+
+    bl = BinaryLabel(root)
+    bl.grid()
+    ToolTip(bl, 'some text')
 
     assert True
