89ecf450d1d09552968c848bdddcaa72fa38b0af
Jason R. Jones <slightlynybbled@gmail.com>
2018-05-21 13:18:52 -0400

    Adding seven segment tests

---
+++ tests/test_SevenSegment.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+import pytest
+
+from tk_tools import SevenSegmentDigits
+
+from tests.test_basic import root
+
+
+@pytest.fixture
+def ssd(root):
+    ssd_widget = SevenSegmentDigits(root, digits=3)
+    ssd_widget.grid()
+
+    yield ssd_widget
+
+
+def test_creation(root):
+    SevenSegmentDigits(root)
+
+
+def test_set_value_str(ssd):
+    ssd.set_value('987')
+
+
+def set_value_int(ssd):
+    ssd.set_value(987)
+
+
+def set_value_float(ssd):
+    ssd.set_value(98.7)
