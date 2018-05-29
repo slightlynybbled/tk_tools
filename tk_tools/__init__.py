31af28ffd931eda90ad29b18c2cfd543b3290a1d
Aoki Ahishatsu <32902621+AokiAhishatsu@users.noreply.github.com>
2018-05-29 12:51:34 +0200

    - Added missing compatibility references in __init__.py
    Signed-off-by: Aoki Ahishatsu <32902621+AokiAhishatsu@users.noreply.github.com>

--- tk_tools/__init__.py
+++ tk_tools/__init__.py
@@ -3,7 +3,7 @@ from tk_tools.groups import EntryGrid, LabelGrid, \
     KeyValueEntry, SpreadSheetReader, ButtonGrid, \
     Calendar, MultiSlotFrame, SevenSegment, SevenSegmentDigits
 from tk_tools.widgets import SmartOptionMenu, SmartSpinBox, \
-    SmartCheckbutton, BinaryLabel
+    SmartCheckbutton, BinaryLabel, ByteLabel
 from tk_tools.tooltips import ToolTip
 
 from tk_tools.version import __version__
@@ -15,6 +15,6 @@ __all__ = [
     'SpreadSheetReader', 'SmartOptionMenu', 'SmartSpinBox',
     'SmartCheckbutton', 'Calendar', 'MultiSlotFrame',
     'SevenSegment', 'SevenSegmentDigits',
-    'BinaryLabel', 'ToolTip',
+    'BinaryLabel', 'ByteLabel', 'ToolTip',
     '__version__'
 ]
