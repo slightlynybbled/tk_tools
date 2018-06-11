dde2be4f6a0484f23756009ceaa424de084879ba
naejgz <40000069+naejgz@users.noreply.github.com>
2018-06-11 09:49:33 +0200

    For flake8
    Modified too long lines

--- tk_tools/canvas.py
+++ tk_tools/canvas.py
@@ -305,9 +305,6 @@ class Gauge(ttk.Frame):
 
         self._redraw()
 
-
-
-
 # Gauge2 : modifications from Gauge
 #          ************************
 # 3 numeric values added : min, center, max,
@@ -321,7 +318,6 @@ class Gauge(ttk.Frame):
 # suppressed integer conversion on extend to not accumulate decimal loss errors
 # idem on value
 # math correction now min_value can be different from 0 or negative
-
 
 class Gauge2(ttk.Frame):
     """
@@ -342,10 +338,12 @@ class Gauge2(ttk.Frame):
     :parm red: the beginning of the red (danger) zone in percent
     :parm yellow_low: in percent warning for low values
     :parm red_low: in percent if very low values are a danger
+    :param bg: background
     """
     def __init__(self, parent, width=200, height=100,
                  min_value=0.0, max_value=100.0, label='', unit='',
-                 divisions=8, yellow=50, red=80, yellow_low=0, red_low=0, bg='lightgrey'):
+                 divisions=8, yellow=50, red=80, yellow_low=0,
+                 red_low=0, bg='lightgrey'):
         self._parent = parent
         self._width = width
         self._height = height
@@ -362,30 +360,26 @@ class Gauge2(ttk.Frame):
 
         super().__init__(self._parent)
 
-        self._canvas = tk.Canvas(self, width=self._width, height=self._height, bg=bg)
-        # ,bg='lavender'
+        self._canvas = tk.Canvas(self, width=self._width,
+                                 height=self._height, bg=bg)
         self._canvas.grid(row=0, column=0, sticky='news')
-
         self._min_value = EngNumber(min_value)
         self._max_value = EngNumber(max_value)
         self._value = self._min_value
-
         self._redraw()
 # ----------------------------------------------------------------------
 
     def _redraw(self):
         self._canvas.delete('all')
-
         max_angle = 120.0
-        value_as_percent = (self._value - self._min_value) / (self._max_value - self._min_value)
+        value_as_percent = ((self._value - self._min_value) /
+                            (self._max_value - self._min_value))
         value = float(max_angle * value_as_percent)
         # no int() => accuracy
-
         # create the tick marks and colors across the top
         for i in range(self._divisions):
             extent = (max_angle / self._divisions)
             start = (150.0 - i * extent)
-
             rate = (i+1)/(self._divisions+1)
             if rate < self._red_low:
                 bg_color = 'red'
@@ -404,10 +398,8 @@ class Gauge2(ttk.Frame):
                 start=start, extent=-extent, width=2,
                 fill=bg_color, style='pie'
             )
-
         bg_color = 'white'
         red = '#c21807'
-
         ratio = 0.06
         self._canvas.create_arc(self._width * ratio,
                                 int(self._height * 0.25),
@@ -415,7 +407,6 @@ class Gauge2(ttk.Frame):
                                 int(self._height * 1.8 * (1.0 - ratio * 1.1)),
                                 start=150, extent=-120, width=2,
                                 fill=bg_color, style='pie')
-
         # readout & title
         self.readout(self._value, 'black')  # BG black if OK
 
@@ -434,7 +425,6 @@ class Gauge2(ttk.Frame):
         self._canvas.create_text(
             self._width * 0.5, self._height * 0.1,
             font=('Courier New', 10), text=value_text)
-
         # create first half (red needle)
         self._canvas.create_arc(0, int(self._height * 0.15),
                                 self._width, int(self._height * 1.8),
@@ -446,7 +436,6 @@ class Gauge2(ttk.Frame):
                                 self._width, int(self._height * 1.8),
                                 start=30, extent=120-value, width=3,
                                 outline=red)'''
-
         # create inset red
         self._canvas.create_arc(self._width * 0.35, int(self._height * 0.7),
                                 self._width * 0.65, int(self._height * 1.2),
@@ -481,18 +470,14 @@ class Gauge2(ttk.Frame):
         self._canvas.create_text(
             self._width * 0.5, self._height * 0.5 + r_offset,
             font=('Courier New', 10), text=value_text, fill='white')
-
 # ----------------------------------------------------------------------
 
     def set_value(self, value):
         self._value = EngNumber(value)
         if self._min_value * 1.02 < value < self._max_value * 0.98:
             self._redraw()      # refresh all
-        else:                             # OFF limits refresh only readout
+        else:                   # OFF limits refresh only readout
             self.readout(self._value, 'red')  # on RED BackGround
-
-
-
 
 
 
